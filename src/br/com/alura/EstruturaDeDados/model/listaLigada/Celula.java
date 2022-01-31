4c7da6ac8f9113b2fb91459805cd755234e11104
Lucas da Silva Santos <lucas.parkl@outlook.com>
2022-01-31 18:10:51 -0300

    WIP: Estrutura de Dados: finalização do modulo 3: - implementação de lista ligada simples.

---
+++ src/br/com/alura/EstruturaDeDados/model/listaLigada/Celula.java
@@ -0,0 +1,33 @@
+package br.com.alura.EstruturaDeDados.model.listaLigada;
+
+public class Celula {
+
+    private  Object elemento;
+    private Celula proxima;
+
+    public Celula(Object elemento, Celula proxima) {
+        this.elemento = elemento;
+        this.proxima = proxima;
+    }
+
+    public Object getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(Object elemento) {
+        this.elemento = elemento;
+    }
+
+    public Celula getProxima() {
+        return proxima;
+    }
+
+    public void setProxima(Celula proxima) {
+        this.proxima = proxima;
+    }
+
+    @Override
+    public String toString() {
+        return elemento.toString();
+    }
+}
---
+++ src/br/com/alura/EstruturaDeDados/model/teste/testaListaLigada.java
@@ -0,0 +1,36 @@
+package br.com.alura.EstruturaDeDados.model.teste;
+
+import br.com.alura.EstruturaDeDados.model.listaLigada.ListaLigada;
+
+public class testaListaLigada {
+
+    public static void main(String[] args) {
+        ListaLigada listaLigada = new ListaLigada();
+
+
+        listaLigada.adicionaNoComeço("1");
+        listaLigada.adicionaNoComeço("2");
+        listaLigada.adicionaNoComeço("3");
+        System.out.println(listaLigada);
+
+        listaLigada.adiciona("4");
+        listaLigada.adiciona("5");
+
+        System.out.println(listaLigada);
+
+        listaLigada.adiciona(2,"6");
+        System.out.println(listaLigada);
+
+        listaLigada.remove(5);
+        System.out.println(listaLigada);
+
+        listaLigada.adiciona(5,'7');
+        System.out.println(listaLigada);
+
+        System.out.println(listaLigada.contem("2"));
+        System.out.println(listaLigada.contem("9"));
+
+        listaLigada.removerDoComeco();
+        System.out.println(listaLigada);
+    }
+}
